657a89db7e0f9ff5f78c9f57b7286fbc9e0b9a92
akka-tech <akkaabdlwahd3@gmail.com>
2025-08-01 18:04:27 +0200

    Clean up code a bit

--- alx-project-0x03/interfaces/index.ts
+++ alx-project-0x03/interfaces/index.ts
@@ -1,48 +1,14 @@
-export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+import { ReactNode } from "react"
+
+export interface PageRouteProps {
+  pageRoute: string
 }
-
-export interface UserProps {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
 }
-
-export interface PostData {
-  userId: number;
-  id?: number;
-  title: string;
-  body: string;
-}
-
-export interface PostModalProps {
-  onClose: () => void;
-  onSubmit: (post: PostData) => void;
-}
-
-
-export interface UserModalProps {
-  user: UserProps
-  onSubmit: (post: UserProps) => void
+export interface LayoutProps {
+  children: ReactNode;
 }
