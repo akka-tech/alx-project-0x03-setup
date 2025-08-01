387316220070b228d5458679d1ae4f2d403bb6e1
akka-tech <akkaabdlwahd3@gmail.com>
2025-08-01 17:43:53 +0200

    create nextjs App

---
+++ alx-project-0x03/pages/_app.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
